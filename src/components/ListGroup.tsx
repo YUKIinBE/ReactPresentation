c6e716d80169989c924d8edf3d11514bd1b81758
YUKI <yuki.marchal@edu.technobel.be>
2024-09-03 11:49:03 +0200

    Render a list dynamically

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,28 +1,20 @@
-import { Fragment } from "react/jsx-runtime";
-
 function ListGroup() {
-	return (
-		//* function can return only 1 html element so we use <div>
-		// <div>
-		// 	<h1>List</h1>
-		// 	<ul className="list-group">
-		// 		<li className="list-group-item">An item</li>
-		// 		<li className="list-group-item">A second item</li>
-		// 		<li className="list-group-item">A third item</li>
-		// 		<li className="list-group-item">A fourth item</li>
-		// 		<li className="list-group-item">And a fifth one</li>
-		// 	</ul>
-		// </div>
+	const items = [
+		'New York',
+		'San Francisco',
+		'Tokyo',
+		'London',
+		'Paris'
+	];
 
-		//* empty bracket is better as we don't actually need a <div>
+
+	return (
 		<>
 			<h1>List</h1>
 			<ul className="list-group">
-				<li className="list-group-item">An item</li>
-				<li className="list-group-item">A second item</li>
-				<li className="list-group-item">A third item</li>
-				<li className="list-group-item">A fourth item</li>
-				<li className="list-group-item">And a fifth one</li>
+				//* React needs a key to track elements
+				//* .map is a TS function so we need {}
+				{items.map(item => <li key={item}>{item}</li>)}
 			</ul>
 		</>
 	);
