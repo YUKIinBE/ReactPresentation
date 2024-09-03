45241f90a9054d2bb63c7ae9b05bf6782e372730
YUKI <yuki.marchal@edu.technobel.be>
2024-09-03 15:54:18 +0200

    Build a Button Component

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface Props {
+	children: string;
+	//* limit the choice null / primary / secondary / danger
+	color?: 'primary' | 'secondary' | 'danger';
+	onClick: () => void
+}
+
+
+function Button({children, onClick, color = 'primary'}: Props) {
+	return (
+		<button className={'btn btn-' + color} onClick={onClick}>{children}</button>
+	)
+}
+
+export default Button
