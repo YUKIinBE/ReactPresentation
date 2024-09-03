39e7fb0235375f49df6c078f796abc461a80d909
YUKI <yuki.marchal@edu.technobel.be>
2024-09-03 12:04:26 +0200

    Render a message when there is no element in the list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,31 @@
 function ListGroup() {
-	const items = [
+	let items = [
 		'New York',
 		'San Francisco',
 		'Tokyo',
 		'London',
 		'Paris'
 	];
+	items = [];
+
+
+
+	//* <h1> is repeated. to avoid this => 
+	// if (items.length === 0)
+	// 	return (
+	// 		<>
+	// 			<h1>List</h1>
+	// 			<p>No item found</p>
+	// 		</>
+	// 	);
 
 
 	return (
 		<>
 			<h1>List</h1>
+			{//* we use ternary operator
+				items.length === 0 ? <p>No item found</p> : null}
 			<ul className="list-group">
-				//* React needs a key to track elements
-				//* .map is a TS function so we need {}
 				{items.map(item => <li key={item}>{item}</li>)}
 			</ul>
 		</>
