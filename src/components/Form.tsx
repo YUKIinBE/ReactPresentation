4b5ea744e2ad94e04908afbde572d4797fe0bd8e
Yuki <yuki.marchal@edu.technobel.be>
2024-09-15 20:59:18 +0200

    applying validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,32 +1,49 @@
-import React, { FormEvent, useRef, useState } from 'react';
-import {FieldValue, FieldValues, useForm} from 'react-hook-form'
+import React, { FormEvent, useRef, useState } from "react";
+import { FieldValue, FieldValues, useForm } from "react-hook-form";
+
+interface FormData {
+  name: string;
+  age: number;
+}
 
 const Form = () => {
-  const {register,handleSubmit} = useForm();
-  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
   const onSubmit = (data: FieldValues) => console.log(data);
-  
+
   return (
-    <form onSubmit={handleSubmit(data => console.log(data))}>
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input 
-        {...register('name')}
-        id="name" 
-        type="text" 
-        className="form-control" />
+        <input
+          {...register("name", { required: true, minLength: 3 })}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is required.</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">The name must be at least 3 characters.</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input 
-        {...register('age')}
-        id="age" 
-        type="number" 
-        className="form-control" />
+        <input
+          {...register("age")}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
