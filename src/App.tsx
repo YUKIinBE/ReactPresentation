25c148d8d36b3a3b719a97a064bc2386bd430260
Yuki <yuki.marchal@edu.technobel.be>
2024-09-15 19:56:54 +0200

    building a form

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import Heart from "./components/Heart";
-import Customer from "./components/customer";
-import Navbar from "./components/Navbar";
-import Cart from "./components/Cart";
-import Player from "./components/player";
+import Form from "./components/Form";
+
 
 function App() {
-  const [cartItems, setCartItems] = useState(['Product1', 'Product2'])
-
-  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      {alertVisible && <Alert onClose={()=> setAlertVisibility(false)}>My Alert</Alert>}
-      <Button
-        color="primary"
-        onClick={() => {
-          setAlertVisibility(true)
-        }}
-      >My Button</Button>
-
-      <Heart onClick={() => console.log('clicked') }/>
-        <Navbar cartItemsCount={cartItems.length}/>
-        <Cart cartItems={cartItems} onClear={() => setCartItems([])}/>
+      <Form />
     </div>
   )
 }
