d0a9f9d2efa47bb7c0d9fef0393d9bb4d72f7013
Yuki <yuki.marchal@edu.technobel.be>
2024-09-16 15:40:30 +0200

    fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import { useEffect, useRef, useState } from "react";
-import Form from "./components/Form";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import categories from "./expense-tracker/categories";
-import ProductList from "./components/ProductList";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const connect = () => console.log('Connecting');
-const disconnect = () => console.log('Disconnecting');
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
-    connect();
-
-    return () => disconnect();
-  })
+    axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .then(response => setUsers(response.data))
+  }, []);
   return (
-    <div>
-    </div>
+    <ul>
+      {users.map(user => <li key={user.id}>{user.name}</li>)}
+    </ul>
   );
 }
 
